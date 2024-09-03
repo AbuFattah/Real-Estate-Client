014c78722b876e574e078b523285f806dd0e0cfd
Abu Fattah <mdnahid142493@gmail.com>
2024-09-03 17:02:09 +0600

    Create Linter for Node

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     "react/prop-types": "off",
-    "no-unused-vars": "error",
+    "no-unused-vars": "off",
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
